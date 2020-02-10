583f7b3522d1970cce225e82fd6f012ddcec7374
Soporte <soporte@vincco.dom>
2020-02-10 13:12:32 -0700

    .

---
+++ curso-udemy/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Eliott Ivan Salazar Quijada</h2>
+        <h3>Vamos a aprender React</h3>
+        <p>
+          Hola Mundo, estoy aprendiendo a utilizar React
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
